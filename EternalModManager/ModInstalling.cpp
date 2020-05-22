799fc5ec744635006c6d5577ba3e13ba77adeab5
SpaceMaN1210 <56092944+SpaceMaN1210@users.noreply.github.com>
2020-05-22 11:15:09 +0300

    fix:fixed form

--- EternalModManager/ModInstalling.cpp
+++ EternalModManager/ModInstalling.cpp
@@ -1,8 +1,20 @@
 #include "ModInstalling.h"
 
-ModInstalling::ModInstalling(QWidget *parent) : QWidget(parent)
+ModInstalling::ModInstalling(QString modName, QWidget *parent) : QWidget(parent)
 {
 	ui.setupUi(this);
+
+	modName_ = modName;
+
+	ModManager* manager = new ModManager;
+
+	ui.descriptionLabel->setText(manager->getModDescription(modName_));
+	ui.modulNameLabel->hide();
+	ui.modNameLabel->setText(modName_ + " " + manager->getModVersion(modName_));
+	ui.imageLabel->setPixmap(QPixmap(manager->getModImagePath(modName_)));
+	this->setWindowTitle(modName_);
+	this->setWindowIcon(QIcon(":/EternalModManager/Resources/eternal.png"));
+	this->setFixedSize(QSize(this->width(), this->height()));
 }
 
 
--- EternalModManager/ModInstalling.h
+++ EternalModManager/ModInstalling.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "ui_ModInstalling.h"
+#include "ModManager.h"
+#include "KeyWords.h"
 #include <QtWidgets>
 
 class ModInstalling : public QWidget
 {
 	Q_OBJECT
 public:
-	explicit ModInstalling(QWidget *parent = nullptr);
+	explicit ModInstalling(QString modName, QWidget *parent = nullptr);
 	~ModInstalling();
 private:
 	Ui::ModInstallingForm ui;
+	QString modName_;
 };
 
